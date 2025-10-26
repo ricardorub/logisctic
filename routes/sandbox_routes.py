9eeb31e53f169e6f356d5acdd80ba4a799441dd4
ricardorub <65383995+ricardorub@users.noreply.github.com>
2025-10-26 11:48:40 -0500

    Delete routes directory

--- routes/sandbox_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-sandbox_bp = Blueprint('sandbox', __name__)
-
-@sandbox_bp.route('/sandbox')
-def sandbox_page():
-    return render_template('sandbox.html')
