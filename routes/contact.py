848bb7581496805c29b1ea92e498dac43ed1aa51
ricardorub <65383995+ricardorub@users.noreply.github.com>
2025-10-30 21:34:29 -0500

    Delete routes directory

--- routes/contact.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from controller import contact_controller
-
-contact_bp = Blueprint('contact', __name__)
-
-contact_bp.route('/api/contact', methods=['POST'])(contact_controller.submit_contact)
